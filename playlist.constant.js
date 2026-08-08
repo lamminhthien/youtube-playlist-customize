71b31fdca76d8a5c99dc61c2b6cec9a84d67fb56
Thien Lam <c062201@yara.com>
2026-08-08 16:25:19 +0700

    Add playlist mùa hè rực rỡ

--- playlist.constant.js
+++ playlist.constant.js
@@ -1,4 +1,5 @@
 export const PLAY_LIST_ID = {
     // 'CO_GAI_CUA_ANH' : 'PLEyKu1JwbU4te4H7bkxp30Fx8ZmsP42Av',
-    'TLM_SHARED_2026' : 'PLFM_5xRQL7MQ'
+    'TLM_SHARED_2026' : 'PLFM_5xRQL7MQ',
+    'Mùa Hè Rực Rỡ': "PLEyKu1JwbU4vO5WsW6c9qjF--8jvwbb78"
 }
