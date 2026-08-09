933151efc7c902af105329581afb5e9f96f7b5ba
Thien Lam <lamminhthien02012000@gmail.com>
2026-08-09 20:26:47 +0700

    Update playlist.js

--- constants/playlist.js
+++ constants/playlist.js
@@ -1,7 +1,7 @@
 export const PLAY_LIST_ID = {
     // 'CO_GAI_CUA_ANH' : 'PLEyKu1JwbU4te4H7bkxp30Fx8ZmsP42Av',
-    'TLM_Shared_2026' : 'PLFM_5xRQL7MQ',
     'TLM_Shared_001_2026': 'PLe9Hwhr-w510',
+    'TLM_Shared_2026' : 'PLFM_5xRQL7MQ',
     'Mùa hè rực rỡ': "PLEyKu1JwbU4vO5WsW6c9qjF--8jvwbb78",
     "Mix VN Music": "RD5e7e_KZINA4"
 }
