a6c05df44cd4b33a2035c77e30542f50d22afdb2
Thiện Lâm <c062201@yara.com>
2026-08-30 20:35:31 +0700

    Update renderReportIssue.js

--- utils/renderReportIssue.js
+++ utils/renderReportIssue.js
@@ -9,7 +9,7 @@ export function renderReportIssueButton(onOpen) {
       <line x1="12" y1="8" x2="12" y2="12" />
       <line x1="12" y1="16" x2="12.01" y2="16" />
     </svg>
-    Report Issue
+    Feedback
   `;
   btn.addEventListener("click", onOpen);
   return btn;
